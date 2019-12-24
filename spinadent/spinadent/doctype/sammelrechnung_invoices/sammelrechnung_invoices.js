28498fb4f0f5f45b001098e66baaf4a875f07dc2
lasalesi <larsericmueller@gmail.com>
2019-12-24 09:29:50 +0100

    doctype redesign

--- spinadent/spinadent/doctype/sammelrechnung_invoices/sammelrechnung_invoices.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, libracore and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Sammelrechnung Invoices', {
-	// refresh: function(frm) {
-
-	// }
-});
