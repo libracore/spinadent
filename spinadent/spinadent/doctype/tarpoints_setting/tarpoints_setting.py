d4492e6df6f5cac1b045b82c9ff7b9976e58ca5f
Chief5 <ben.helmy@gmail.com>
2020-01-17 17:34:06 +0100

    xml structure for template

---
+++ spinadent/spinadent/doctype/tarpoints_setting/tarpoints_setting.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, libracore and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class TarPointsSetting(Document):
+	pass
+
+    # prepare transfer file
+    def render_transfer_file(self):
+            data = {
+                'transactions': self.get_individual_transactions(restrict_currencies)
+            }            
+            
+    # change path to template
+        content = frappe.render_template('erpnextswiss/erpnextswiss/doctype/tarpoints_setting/transfer_file.html', data)
+        return {'content': content}
