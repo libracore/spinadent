c6287e23139f16db0f4261e18abc8db35b4cad4e
Chief5 <ben.helmy@gmail.com>
2020-02-19 23:17:57 +0100

    html for structur and improvements on py code

--- spinadent/spinadent/doctype/tarpoints_setting/tarpoints_setting.py
+++ spinadent/spinadent/doctype/tarpoints_setting/tarpoints_setting.py
@@ -6,13 +6,23 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class TarPointsSetting(Document):
-    # prepare transfer file
-    def render_transfer_file(self):
-        data = {
-            'transactions': self.get_individual_transactions(restrict_currencies)
-        }            
-            
-        # change path to template
-        content = frappe.render_template('erpnextswiss/erpnextswiss/doctype/tarpoints_setting/transfer_file.html', data)
-        return {'content': content}
+@frappe.whitelist()
+def make_tarpoint_file(self):
+
+        data = {}
+        data['xml_version'] = frappe.get_value("ERPNextSwiss Settings", "ERPNextSwiss Settings", "xml_version")
+        data['xml_region'] = frappe.get_value("ERPNextSwiss Settings", "ERPNextSwiss Settings", "banking_region")
+        data['date'] = time.strftime("%Y-%m-%dT%H:%M:%S")                    # creation date and time ( e.g. 2010-02-15T07:30:00 )
+      
+		#rechnungssteller
+        company_address = get_primary_address(target_name=self.company, target_type="Company")
+        data['biller'] = {
+            #data['biller']['company_name'] = frappe.get_doc('self.doctype', self.company )
+            #data['biller']['street'] = cgi.escape(company_address.address_line1)
+            #data['biller']['zip'] = cgi.escape(company_address.pincode)
+            #data['biller']['city'] = cgi.escape(company_address.city)
+            #data['biller']['phone'] = 
+        }
+
+        content = frappe.render_template('spinadent/spinadent/doctype/tarpoints_setting/templateTest.html', data) #renders template and data from database
+        return {'content': content} #returns data
