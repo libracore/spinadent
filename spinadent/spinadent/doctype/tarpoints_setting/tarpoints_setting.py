10e4b660a481c6b3b87aabbeea04d0873a12b2dd
Chief5 <ben.helmy@gmail.com>
2020-03-31 18:14:09 +0200

    added defaults

--- spinadent/spinadent/doctype/tarpoints_setting/tarpoints_setting.py
+++ spinadent/spinadent/doctype/tarpoints_setting/tarpoints_setting.py
@@ -42,19 +42,19 @@ def make_tarpoint_file(qtn=None,so=None, sinv=None, dn=None):
             practitioner_zsr_number = "G999999"
             
         data['biller'] = {
-            'designation' : biller_details.designation,
-            'family_name' : biller_details.first_name,
-            'given_name' : biller_details.last_name,
+            'designation' : biller_details.designation or "",
+            'family_name' : biller_details.first_name or "",
+            'given_name' : biller_details.last_name or "", 
             'street' : biller_address.get('address_line1', ''),
             'statecode' : biller_address.get('county', 'SG')[:2].upper(),
             'zip' : biller_address.get('pincode', '9000'),
             'city' : biller_address.get('city', 'Berschis'),
-            'phone' : biller_details.mobile_phone,
+            'phone' : biller_details.mobile_phone or "",
             'fax' : biller_address.get('fax', ""),
-            'gln_number': practitioner_gln_number,
-            'zsr_number' : practitioner_zsr_number,
-            'tax_id' : biller_details.tax_id,
-            'subaddressing' : biller_details.department
+            'gln_number': practitioner_gln_number or "",
+            'zsr_number' : practitioner_zsr_number or "",
+            'tax_id' : biller_details.tax_id or "",
+            'subaddressing' : biller_details.department or ""
             }
 
         debitor_details = frappe.get_doc('Patient', doc.patient)
@@ -66,37 +66,37 @@ def make_tarpoint_file(qtn=None,so=None, sinv=None, dn=None):
             insurance_gln_number = "G999999"
         
         data['debitor'] = {
-            'company' : insurance.company_name,
-            'street' : insurance.street,
-            'zip' : insurance.pincode, 
-            'city' : insurance.city,
-            'gln_nr' : insurance_gln_number
+            'company' : insurance.company_name or "",
+            'street' : insurance.street or "",
+            'zip' : insurance.pincode or "", 
+            'city' : insurance.city or "",
+            'gln_nr' : insurance_gln_number or ""
             }
             
         provider_details = frappe.get_doc('Healthcare Practitioner', doc.ref_practitioner)
         provider_address = get_primary_address(target_name=doc.ref_practitioner, target_type="Healthcare Practitioner")
          
         data['provider'] = {
-            'designation' : provider_details.designation,
-            'family_name' : provider_details.first_name,
-            'given_name' : provider_details.last_name,
+            'designation' : provider_details.designation or "",
+            'family_name' : provider_details.first_name or "",
+            'given_name' : provider_details.last_name or "",
             'street' : provider_address.get('address_line1', ""),
             'statecode' : provider_address.get('county', "SG")[:2].upper(),
             'zip' : provider_address.get('pincode', ""),
             'city' : provider_address.get('city', ""),
-            'phone' : provider_details.mobile_phone,
+            'phone' : provider_details.mobile_phone or "",
             'fax' : provider_address.get('fax', ""),
-            'gln_number': practitioner_gln_number,
-            'zsr_number' : provider_details.zsr_number,
-            'subaddressing' : provider_details.department
+            'gln_number': practitioner_gln_number or "",
+            'zsr_number' : provider_details.zsr_number or "",
+            'subaddressing' : provider_details.department or ""
             }    
             
         data['insurance'] = {
-            'company' : insurance.company_name,
-            'street' : insurance.street,
-            'zip' : insurance.pincode, 
-            'city' : insurance.city,
-            'gln_nr' : insurance.gln_nummer
+            'company' : insurance.company_name or "",
+            'street' : insurance.street or "",
+            'zip' : insurance.pincode or "", 
+            'city' : insurance.city or "",
+            'gln_nr' : insurance.gln_nummer or ""
             }
             
         patient_details = frappe.get_doc('Patient', doc.patient)
@@ -112,25 +112,25 @@ def make_tarpoint_file(qtn=None,so=None, sinv=None, dn=None):
             salutation = "Herr"
         
         data['patient'] = {
-            'gender' : patient_details.sex,
-            'birthdate' : patient_details.dob,
+            'gender' : patient_details.sex or "",
+            'birthdate' : patient_details.dob or "",
             'salutation' : salutation,
             'family_name' : last_name,
             'given_name' : first_name,
-            'street' : patient_details.street,
-            'country' : patient_details.country,
-            'zip' : patient_details.pincode,
-            'city' : patient_details.city,
-            'ahv_number' : patient_details.ahv_nummer
+            'street' : patient_details.street or "",
+            'country' : patient_details.country or "",
+            'zip' : patient_details.pincode or "",
+            'city' : patient_details.city or "",
+            'ahv_number' : patient_details.ahv_nummer or ""
             }
             
         data['guarantor'] = {
             'salutation' : salutation,
             'family_name' : last_name,
             'given_name' : first_name,
-            'street' : patient_details.street,
-            'zip' : patient_details.pincode,
-            'city' : patient_details.city,
+            'street' : patient_details.street or "",
+            'zip' : patient_details.pincode or "",
+            'city' : patient_details.city or "",
             }
             
         tax_id_toInt = ""  
@@ -141,19 +141,19 @@ def make_tarpoint_file(qtn=None,so=None, sinv=None, dn=None):
         taxless_amount = (doc.net_total) - (doc.total_taxes_and_charges)
        
         data['balance'] = {
-            'currency' : doc.currency,
-            'net_total' : doc.net_total,
+            'currency' : doc.currency or "",
+            'net_total' : doc.net_total or "",
             'tax_id' : tax_id_toInt,
             'taxless_total' : taxless_amount, 
-            'total_taxes' : doc.total_taxes_and_charges 
+            'total_taxes' : doc.total_taxes_and_charges or ""
             } 
               
         creditor_details = frappe.get_doc('Healthcare Practitioner', doc.ref_practitioner)
         creditor_address = get_primary_address(target_name=doc.ref_practitioner, target_type="Healthcare Practitioner")
         data['creditor'] = {
-            'designation' : creditor_details.designation,
-            'family_name' : creditor_details.first_name,
-            'given_name' : creditor_details.last_name,
+            'designation' : creditor_details.designation or "",
+            'family_name' : creditor_details.first_name or "",
+            'given_name' : creditor_details.last_name or "",
             'street' : creditor_address['address_line1'],
             'country_code' : creditor_address['country_code'], 
             'zip' : creditor_address['pincode'],
